1dc53d4cad1c174ee20994e585eefc0f629a6429
chrismabon <christopherjmabon@gmail.com>
2018-10-03 01:06:23 -0400

    Documented and reworked List class

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @title Linked List
+ * @project linked_list
+ * @filename main.cpp
+ * @github https://github.com/chrismabon/linked_list
+ * @author Chris Mabon
+ *
+ * TODO
+ *
+ */
+
+#include "test.h"
+
+
+int main(int argc, char** argv) {
+
+    return run_tests();
+}
